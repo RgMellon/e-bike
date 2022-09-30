cf51ea1a9058629e43a105fa975d190d8f69e441
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-09-30 19:57:20 -0300

    feat: add theme

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NativeBaseProvider, Box } from "native-base";
+import { SafeAreaView } from "react-native";
 
+import { theme } from './src/styles/theme'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={theme}>
+      <SafeAreaView style={{flex: 1, backgroundColor: theme.colors.ocean}}>
+        <Box>Hello world</Box>
+      </SafeAreaView>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
